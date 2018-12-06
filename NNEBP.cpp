b2352fba080af1f3eebf9fb49720a571939aafc0
Eunggu Yun <yuneg11@gmail.com>
2018-12-06 23:50:25 +0900

    Add NeuralNetwork constructor and destructor

--- NNEBP.cpp
+++ NNEBP.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 
 class NeuralNetwork {
 private:
-    int i;
-    int j;
-    int k;
-    double **input_ni;
-    double **output_nk;
-    double learning_rate;
+    int i, j, k;
     double **weight_ji;
     double **weight_kj;
-    int iteration;
 public:
-    NeuralNetwork(int i, int j, int k);
-    setData(double **input_data, double **output_data);
-    iterate(int num);
-    getOutput(double **input);
+    NeuralNetwork(int _i, int _j, int _k);
     ~NeuralNetwork();
 };
+
+NeuralNetwork::NeuralNetwork(int _i, int _j, int _k): i(_i), j(_j), k(_k) {
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution(-0.01, 0.01);
+
+    weight_ji = new double*[j];
+    for(int tj = 0; tj < j; tj++) {
+        weight_ji[tj] = new double[i];
+        for(int ti = 0; ti < i; ti++)
+            weight_ji[tj][ti] = distribution(generator);
+    }
+
+    weight_kj = new double*[k];
+    for(int tk = 0; tk < k; tk++) {
+        weight_kj[tk] = new double[j];
+        for(int tj = 0; tj < j; tj++)
+            weight_kj[tk][tj] = distribution(generator);
+    }
+}
+
+NeuralNetwork::~NeuralNetwork() {
+    for(int tj = 0; tj < j; tj++)
+        delete weight_ji[tj];
+    delete weight_ji;
+
+    for(int tk = 0; tk < k; tk++)
+        delete weight_kj[tk];
+    delete weight_kj;
+}
 
 int main() {
     return 0;
