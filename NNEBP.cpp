d6ec382afba2f346f36f9a1e167fba9c9679b8af
Eunggu Yun <yuneg11@gmail.com>
2018-12-07 00:43:13 +0900

    Add NeuralNetwork getNeuronOutput function

--- NNEBP.cpp
+++ NNEBP.cpp
@@ -8,6 +8,7 @@ private:
     double **weight_ji;
     double **weight_kj;
     double activation(double x);
+    double getNeuronOutput(int num, double input[], double weight[]);
 public:
     NeuralNetwork(int _i, int _j, int _k);
     ~NeuralNetwork();
@@ -19,21 +20,31 @@ NeuralNetwork::NeuralNetwork(int _i, int _j, int _k): i(_i), j(_j), k(_k) {
 
     weight_ji = new double*[j];
     for(int tj = 0; tj < j; tj++) {
-        weight_ji[tj] = new double[i];
-        for(int ti = 0; ti < i; ti++)
+        weight_ji[tj] = new double[i+1];
+        for(int ti = 0; ti <= i; ti++)
             weight_ji[tj][ti] = distribution(generator);
     }
 
     weight_kj = new double*[k];
     for(int tk = 0; tk < k; tk++) {
-        weight_kj[tk] = new double[j];
-        for(int tj = 0; tj < j; tj++)
+        weight_kj[tk] = new double[j+1];
+        for(int tj = 0; tj <= j; tj++)
             weight_kj[tk][tj] = distribution(generator);
     }
 }
 
 double NeuralNetwork::activation(double x) {
     return 1 / (1 + exp(-x)); // Sigmoid
+}
+
+double NeuralNetwork::getNeuronOutput(int num, double input[], double weight[]) {
+    double net = 0;
+
+    for(int ti = 0; ti < num; ti++)
+        net += input[ti] * weight[ti];
+    net += weight[num];
+
+    return activation(net);
 }
 
 NeuralNetwork::~NeuralNetwork() {
